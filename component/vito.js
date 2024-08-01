568c17e5631e0e7759c423fdb97f026a1ea7817c
Kavinda-liyan <110520736+Kavinda-liyan@users.noreply.github.com>
2024-08-01 22:13:13 +0530

    new

--- component/vito.js
+++ component/vito.js
@@ -1,3 +1,6 @@
+let small=document.getElementById("smallP");
+let medium=document.getElementById("mediumP");
+let large=document.getElementById("largeP");
 let submitOrder=document.getElementById("SubmitOrderBtn");
 let Cheese=document.getElementById("checkCheese");
 let Olive=document.getElementById("checkOlive");
@@ -5,9 +8,10 @@ let Peporoni=document.getElementById("checkPeporoni");
 let Allmeat=document.getElementById("checkMeat");
 let Bpork=document.getElementById("checkBpork");
 let emptyError=document.getElementById("emptyFill2");
+let emptyError1=document.getElementById("emptyFill1");
 let withToppingPrice;
 
-let quantity=document.getElementById("quantity");
+let count=document.getElementById("quantity");
 let increBtn=document.getElementById("increBtn");
 let decreBtn=document.getElementById("decreBtn");
 let incre=0;
@@ -15,12 +19,15 @@ let quantityDisplay;
 vitodiscriptions=document.getElementById("VitoDescription");
 vitoprices=document.getElementById("VitoPrice");
 vitoQuantity=document.getElementById("Vitoquantity");
+let errorChoice=document.getElementById("errorChoice");
+let basePrice;
+let orderNo=document.getElementById("orderNo");
 
 increBtn.onclick=function(){
     
     incre++;
     if(0<incre){
-        quantity.value=incre;
+        count.value=incre;
         
     }
     
@@ -28,7 +35,7 @@ increBtn.onclick=function(){
 decreBtn.onclick=function(){
     incre--;
     if(0<incre){
-        quantity.value=incre;
+        count.value=incre;
     }
    
 }
@@ -37,51 +44,156 @@ decreBtn.onclick=function(){
 
 submitOrder.onclick=function(){
 
-   if(Cheese.checked==false && Olive.checked==false && Peporoni.checked==false 
-    && Allmeat.checked==false && Bpork.checked==false){
-        emptyError.textContent="Please Select Your Toppings and Quantity first before Checkout!";
+    
+
+
+if(small.checked==true || medium.checked==true || large.checked==true){
+
+    errorChoice.textContent="";
+
+    if(small.checked==true){
+        basePrice=1200;
     }
-    else if(Cheese.checked==true && Olive.checked==false && Peporoni.checked==false 
+    else if(medium.checked==true){
+        basePrice=1650;
+    }
+    else if(large.checked==true){
+        basePrice=2100;
+    }
+else{
+errorChoice.textContent="Please select pizza size first";
+
+}
+
+    if(Cheese.checked==false && Olive.checked==false && Peporoni.checked==false 
         && Allmeat.checked==false && Bpork.checked==false){
-
-            if(0<quantity.value){
-                withToppingPrice=(1200+150)*quantity.value;
-                vitodiscriptions.textContent=`Your order is Vito Cheese Pizza`;
-                vitoQuantity.textContent=`Quantity: ${quantity.value}`;
-                vitoprices.textContent=`Price : Rs.${withToppingPrice}`;
-
-            }
-            else{
-                emptyError.textContent="Please select Quantity of food item first before Checkout!";
-            }
-            
-        
-            }
-
-    else if(Cheese.checked==true && Olive.checked==true && Peporoni.checked==false 
-        && Allmeat.checked==false && Bpork.checked==false){
-            if(0<quantity.value){
-                withToppingPrice=(1200+250+150)*quantity.value;
-                vitodiscriptions.textContent=`Your order is Vito Cheese & Olive Pizza`;
-                vitoQuantity.textContent=`Quantity: ${quantity.value}`;
-                vitoprices.textContent=`Price : (1200+250+150)x ${quantity.value} = Rs.${withToppingPrice}`;
-
-            }
-            else{
-                emptyError.textContent="Please select Quantity of food item first before Checkout!";
-            }
+            emptyError.textContent="Please Select Your Toppings and Quantity first before Checkout!";
         }
-    else if(Cheese.checked==true && Olive.checked==true && Peporoni.checked==true 
-        && Allmeat.checked==false && Bpork.checked==false){
-
-        }
-        else if(Cheese.checked==false && Olive.checked==false && Peporoni.checked==false 
-            && Allmeat.checked==true && Bpork.checked==false){
-                
-            }
-            else if(Cheese.checked==false && Olive.checked==false && Peporoni.checked==false 
-                && Allmeat.checked==false && Bpork.checked==true){
-                    
+        else if(Cheese.checked==true && Olive.checked==false && Peporoni.checked==false 
+            && Allmeat.checked==false && Bpork.checked==false){
+    
+                if(0<quantity.value){
+                    orderNo.textContent=`Order No: ${Math.trunc((Math.random()*1000))}` ;
+                    withToppingPrice=(basePrice+150)*count.value;
+                    vitodiscriptions.textContent=`Your order is Vito Cheese Pizza`;
+                    vitoQuantity.textContent=`Quantity: ${count.value}`;
+                    vitoprices.textContent=`Price : (${basePrice}+150)x ${count.value} = Rs.${withToppingPrice}`;
+    
                 }
-        }
+                else{
+                    emptyError1.textContent="Please select Quantity of food item first before Checkout!";
+                }
+                
+            
+                }
+    
+        else if(Cheese.checked==true && Olive.checked==true && Peporoni.checked==false 
+            && Allmeat.checked==false && Bpork.checked==false){
+                if(0<quantity.value){
+                    orderNo.textContent=`Order No: ${Math.trunc((Math.random()*1000))}` ;
+                    withToppingPrice=(basePrice+250+150)*count.value;
+                    vitodiscriptions.textContent=`Your order is Vito Cheese & Olive Pizza`;
+                    vitoQuantity.textContent=`Quantity: ${count.value}`;
+                    vitoprices.textContent=`Price : (${basePrice}+250+150)x ${count.value} = Rs.${withToppingPrice}`;
+    
+                }
+                else{
+                    emptyError1.textContent="Please select Quantity of food item first before Checkout!";
+                }
+            }
+        else if(Cheese.checked==true && Olive.checked==true && Peporoni.checked==true 
+            && Allmeat.checked==false && Bpork.checked==false){
+                if(0<quantity.value){
+                    orderNo.textContent=`Order No: ${Math.trunc((Math.random()*1000))}` ;
+                    withToppingPrice=(basePrice+250+150+250)*count.value;
+                    vitodiscriptions.textContent=`Your order is Vito Cheese, Olive with Peporoni Pizza`;
+                    vitoQuantity.textContent=`Quantity: ${count.value}`;
+                    vitoprices.textContent=`Price : (${basePrice}+250+150+250)x ${count.value} = Rs.${withToppingPrice}`;
+    
+                }
+                else{
+                    emptyError1.textContent="Please select Quantity of food item first before Checkout!";
+                }
+            }
+            else if(Cheese.checked==false && Olive.checked==false && Peporoni.checked==true 
+                && Allmeat.checked==true && Bpork.checked==false){
+                    if(0<quantity.value){
+                        orderNo.textContent=`Order No: ${Math.trunc((Math.random()*1000))}` ;
+                        withToppingPrice=(basePrice+250)*count.value;
+                        vitodiscriptions.textContent=`Your order is Vito Peporoni Pizza`;
+                        vitoQuantity.textContent=`Quantity: ${count.value}`;
+                        vitoprices.textContent=`Price : (${basePrice}+250)x ${count.value} = Rs.${withToppingPrice}`;
+        
+                    }
+                    else{
+                        emptyError1.textContent="Please select Quantity of food item first before Checkout!";
+                    }
+                }
+                else if(Cheese.checked==false && Olive.checked==true && Peporoni.checked==false 
+                    && Allmeat.checked==false && Bpork.checked==false){
+                        if(0<quantity.value){
+                            orderNo.textContent=`Order No: ${Math.trunc((Math.random()*1000))}` ;
+                            withToppingPrice=(basePrice+250)*count.value;
+                            vitodiscriptions.textContent=`Your order is Vito Olive Pizza`;
+                            vitoQuantity.textContent=`Quantity: ${count.value}`;
+                            vitoprices.textContent=`Price : (${basePrice}+250)x ${count.value} = Rs.${withToppingPrice}`;
+            
+                        }
+                        else{
+                            emptyError1.textContent="Please select Quantity of food item first before Checkout!";
+                        }
+                        
+                    }
+
+                    else if(Cheese.checked==false && Olive.checked==false && Peporoni.checked==false 
+                        && Allmeat.checked==true && Bpork.checked==false){
+                            if(0<quantity.value){
+                                orderNo.textContent=`Order No: ${Math.trunc((Math.random()*1000))}` ;
+                                withToppingPrice=(basePrice+450)*count.value;
+                                vitodiscriptions.textContent=`Your order is Vito Allmeat Signature Pizza`;
+                                vitoQuantity.textContent=`Quantity: ${count.value}`;
+                                vitoprices.textContent=`Price : (${basePrice}+450)x ${count.value} = Rs.${withToppingPrice}`;
+                
+                            }
+                            else{
+                                emptyError1.textContent="Please select Quantity of food item first before Checkout!";
+                            }
+                        }
+                        else if(Cheese.checked==false && Olive.checked==false && Peporoni.checked==false 
+                            && Allmeat.checked==false && Bpork.checked==true){
+                                if(0<quantity.value){
+                                    orderNo.textContent=`Order No: ${Math.trunc((Math.random()*1000))}` ;
+                                    withToppingPrice=(basePrice+600)*count.value;
+                                    vitodiscriptions.textContent=`Your order is Vito Sri Lankan Black Pork Signature Pizza`;
+                                    vitoQuantity.textContent=`Quantity: ${count.value}`;
+                                    vitoprices.textContent=`Price : (${basePrice}+600)x ${count.value} = Rs.${withToppingPrice}`;
+                    
+                                }
+                                else{
+                                    emptyError1.textContent="Please select Quantity of food item first before Checkout!";
+                                }
+                            }
+                            else if(Cheese.checked==false && Olive.checked==false && Peporoni.checked==false 
+                                && Allmeat.checked==true && Bpork.checked==true){
+                                    emptyError.textContent="One Type of Signature Pizza under one Order ";
+                                }
+                            else {
+                                emptyError.textContent="Signature Pizzas can not add Toppings please redraw the Order";
+                                vitodiscriptions.textContent=``;
+                                    vitoQuantity.textContent=``;
+                                    vitoprices.textContent=``;
+                                    orderNo.textContent= ``;
+                            }
+}
+else{
+    errorChoice.textContent="Please select pizza size first";
+}
+
+       
+                }
+
+             
+    
+
+   
 
